110ad2337adf6fcce57b2794ee5a2516016cdc67
William Gregorio <william.gregorio.ny@gmail.com>
2025-12-14 15:42:41 -0500

    (manif hotkey) added action key mac and default support

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,16 @@ export default defineConfig({
   manifest: {
     action: {
       default_title: "Export USPS EDDM® routes into a .csv file or quickly copy routes directly into your system's clipboard."
+    },
+    permissions: ["storage"],
+    commands: {
+      "_execute_action": {
+        suggested_key: {
+          default: "Ctrl+Shift+X",
+          mac: "Command+Shift+X"
+        },
+        description: "Open the extension popup"
+      }
     }
   },
   srcDir: "src",
