e0c0abf9c721d63f5a14dfcb3db64b4ec7f98b04
William Gregorio <william.gregorio.ny@gmail.com>
2025-12-14 22:22:26 -0500

    (content init) communicator ready with popup.

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,12 +1,19 @@
+import { Request } from "../../types";
+import { getRowsToExport, extractDataFromRows } from "./utils";
 export default defineContentScript({
   matches: ["https://eddm.usps.com/eddm/select-routes.htm*"],
   main() {
-    browser.runtime.onMessage.addListener((message, _, sendResponse) => {
-      let now = new Date();
-      console.info(now.toISOString())
-      const TABLE_SELECTOR = "table.target-audience-table tbody";
-      console.log(document.querySelectorAll(TABLE_SELECTOR));
-      return true;
+    browser.runtime.onMessage.addListener((message: Request, _, sendResponse) => {
+      if (message.action === "SCRAPE") {
+        try {
+          const rows = getRowsToExport(message.setting);
+          const rawData = extractDataFromRows(rows);
+          sendResponse({ success: true, data: rawData });
+        } catch (error) {
+          const errMessage = error instanceof Error ? error.message : "Error: unknown";
+          sendResponse({ success: false, error: errMessage });
+        }
+      }
     })
   },
 })
